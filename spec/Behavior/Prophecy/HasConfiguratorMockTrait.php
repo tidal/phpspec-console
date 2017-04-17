d17eeab4284fd733f5bab720d9937a66fa38e65c
Timo Michna <timomichna@yahoo.de>
2017-04-17 19:37:12 +0200

    add HasConfiguratorMockTrait

---
+++ spec/Behavior/Prophecy/HasConfiguratorMockTrait.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the phpspec-console package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Tidal\PhpSpec\ConsoleExtension\Behavior\Prophecy;
+
+use Tidal\PhpSpec\ConsoleExtension\Command\InlineConfigurator as Configurator;
+use Prophecy\Prophet;
+use Prophecy\Prophecy\ObjectProphecy;
+
+trait HasConfiguratorMockTrait
+{
+    /**
+     * @return ObjectProphecy
+     */
+    private function createConfiguratorProphecy()
+    {
+        $prophet = new Prophet;
+        return $prophet->prophesize()->willExtend(Configurator::class);
+    }
+
+    /**
+     * @return object|Configurator
+     */
+    private function createConfiguratorMock()
+    {
+        return $this->createConfiguratorProphecy()->reveal();
+    }
+}
+
