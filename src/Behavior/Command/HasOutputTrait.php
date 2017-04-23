ca4c86e01dbe2e8e6d8d1a143f340a07be2145a0
Timo Michna <timomichna@yahoo.de>
2017-04-23 02:42:42 +0200

    add HasOutputTrait

---
+++ src/Behavior/Command/HasOutputTrait.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the phpspec-console package.
+ * (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tidal\PhpSpec\ConsoleExtension\Behavior\Command;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Trait Tidal\PhpSpec\ConsoleExtension\Behavior\Command\HasOutputTrait */
+trait HasOutputTrait
+{
+    /**
+     * @var OutputInterface
+     */
+    protected $input;
+
+    /**
+     * @param OutputInterface $input
+     */
+    public function setOutput(OutputInterface $input)
+    {
+        $this->input = $input;
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    public function getOutput(): OutputInterface
+    {
+        return $this->input;
+    }
+}
+
